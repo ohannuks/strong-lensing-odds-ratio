4aad6fcad7eeef4fffbab030237d4e3041a81dc4
Otto Akseli Hannuksela <oahannuksela@cuhk.edu.hk>
2023-01-25 15:09:38 +0800

    Updated lensed injection creation

--- injections/create_lensed_injections.py
+++ injections/create_lensed_injections.py
@@ -1,7 +1,17 @@
 import ler
-import quintet
-
+import numpy as np
 # Create lensed events with SNR cut-off greater than 8
 lensed_statistics = ler.Lensed()
+nsamples = 100000
+lensed_events = lensed_statistics.create_lensed_images(size=nsamples)
+# Only choose the events with SNR greater than 8
+# Get the network snr of each 4 images for each lensed event
+snrs = lensed_events['network_snr'] # (4, ndim) shape
+# Select the indices where each of the four images has SNR greater than 8
+snr_cut = 8
+indices = np.where(np.all(snrs > snr_cut, axis=0))[0]
+# Select the events with SNR greater than 8
+lensed_events = lensed_events[indices]
 
-quintet.Quintet()
+
+
